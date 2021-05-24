04ec671aa971013a3d236978506624660a99eb8d
Tuan Pham <TuanPham@Tuans-MacBook-Pro.local>
2021-05-24 12:13:51 +0100

    Python Modules

--- python_modules.py
+++ python_modules.py
@@ -3,16 +3,16 @@
 # If the value is 1.49 floor() 1, ceil()1.52 2
 # We have a keyword called import that we can use to call a Module
 
-from random import random
-import math
-# If the user input is >= .50 apply ceil
-# If the user input is <= .49 apply floor
-number = random()
-print(number)
-if number >= 0.50:
-    print(math.ceil(number))
-else:
-    print(math.floor(number))
+# from random import random
+# import math
+# # If the user input is >= .50 apply ceil
+# # If the user input is <= .49 apply floor
+# number = random()
+# print(number)
+# if number >= 0.50:
+#     print(math.ceil(number))
+# else:
+#     print(math.floor(number))
 
 # print(random())
 # num1 = 23.66
@@ -20,3 +20,24 @@ else:
 # print(math.ceil(num1))
 # print(math.floor(num1))
 # What is the use case
+
+# What is the use case
+# os is used to get information about your localhost / your machine such as name, path etc
+# Using a comma, we can use more than one Module on one line
+
+import os, sys
+working_dir = os.getcwd()
+print("This is your current working directory " + working_dir)
+
+system_path = sys.path
+print("This is the path " + str(system_path))
+
+def current_system_path():
+    print("This is your current path ")
+    return sys.path
+print(current_system_path())
+
+def current_working_directory():
+    print("This is your current working directory ")
+    return os.getcwd()
+print(current_working_directory())
